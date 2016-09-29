04db1c9e03b4021c24aa4089224895a5c706abea
jtsoto <jts87@pitt.edu>
2016-09-29 13:47:12 -0400

    take home work 1

--- take home1.py
+++ take home1.py
@@ -1,30 +1,39 @@
 #ask user for preferred measurement system
 x = input("USC or Metric?")
 if x == "usc":
-    uscdis = float(input("How many miles driven?"))
-    uscgas = float(input("How many gallons of gas used?"))
+    uscd = float(input("How many miles driven?"))
+    uscg = float(input("How many gallons of gas used?"))
 #gas and distance conversions from usc to metric
-    metdis = uscdis*1.60934
-    metgas = uscgas*3.78541
-if x == "metric":
-    metdis = float(input("How many kilometers driven?"))
-    metgas = float(input("How many liters of gas used?"))
-#gas and distance conversions from metric to usc
-    uscdis = metdis*.621371
-    uscgas = metgas*.264172
+    metd = uscd*1.60934
+    metg = uscg*3.78541
 #computing fuel consumption
-mpg = uscdis//uscgas
-lpk = 100*metgas//metdis
+    mpg = uscd//uscg
+    lk = 100*metg//metd
+if x == "metric":
+    metd = float(input("How many kilometers driven?"))
+    metg = float(input("How many liters of gas used?"))
+#gas and distance conversions from metric to usc
+    uscd = metd*.621371
+    uscg = metg*.264172
+#computing fuel consumption
+    mpg = uscd//uscg
+    lk = 100*metg//metd
 #putting fuel consumption into categories
-if lpk > 20:
+if lk > 20:
     cc = "extremely poor"
-elif lpk > 15 and lpk <= 20:
+elif lk > 15 and lk <= 20:
     cc = "poor"
-elif lpk > 10 and lpk <=15:
+elif lk > 10 and lk <=15:
     cc = "average"
-elif lpk > 8 and lpk <= 10:
+elif lk > 8 and lk <= 10:
     cc = "good"
-elif lpk <= 8:
+elif lk <= 8:
     cc = "excellent"
+z = " "
+print(28*z,('USC'),14*z,('Metric'))
+print("Distance",13*z,":",format(uscd,'9.3f'),"miles",z,format(metd,'9.3f'),"km")
+print("Gas",18*z,":",format(uscg,'9.3f'),"gallons",z,format(metg,'9.3f'),"liters")
+print("Consumption",10*z,":",format(mpg,'9.3f'),"mpg",z,format(lk,'9.3f'),"1/100km")
+print("Gas Consumption Rating:",cc)
 
 
